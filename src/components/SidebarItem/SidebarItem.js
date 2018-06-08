7bb8b69dce7604426a2efcd46cf642a4f18e8633
Kevin J Dexter <kevin.j.dexter@gmail.com>
2018-06-08 14:55:29 -0500

    locations appear in recently added

--- src/components/SidebarItem/SidebarItem.js
+++ src/components/SidebarItem/SidebarItem.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import Public from '@material-ui/icons/Public';
 import Book from '@material-ui/icons/Book';
+import Location from '@material-ui/icons/LocationCity';
 
 class SidebarItem extends Component {
   render() {
@@ -16,6 +17,10 @@ class SidebarItem extends Component {
       case 'story':
         icon = <Book />
         toDisplay = <Link to={`/view/story/${this.props.item.id}`}>{this.props.item.title} - {icon}</Link>
+        break;
+      case 'location':
+        icon = <Location />
+        toDisplay = <Link to={`/view/story/${this.props.item.id}`}>{this.props.item.name} - {icon}</Link>
         break;
       default:
         break;
