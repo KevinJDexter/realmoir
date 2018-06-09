8214bccfbd4272104d85d3e87ef4f3e254a2032d
Kevin J Dexter <kevin.j.dexter@gmail.com>
2018-06-09 18:14:03 -0500

    added delete routes for location story junction table

--- server/routes/junction.router.js
+++ server/routes/junction.router.js
@@ -22,4 +22,62 @@ router.post('/locationStory', (req, res) => {
     }
 })
 
+router.delete('/locationStory/location/:id', (req, res) => {
+  console.log('DELETE /api/junction/locationStory/location/:id');
+  if (req.isAuthenticated) {
+    let query = `
+      DELETE FROM "locations_stories_junction"
+      WHERE "location_id" = $1
+      AND EXISTS (SELECT 1
+                  FROM "locations_stories_junction" as "ls"
+                  JOIN "locations" AS "l"
+                  ON "l"."id" = "ls"."location_id"
+                  JOIN "worlds" AS "w"
+                  ON "w"."id" = "l"."world_id"
+                  WHERE "ls"."location_id" = $1
+                  AND "w"."user_id" = $2)
+    `;
+    let params = [req.params.id, req.user.id];
+    pool.query(query, params)
+      .then(() => {
+        res.sendStatus(202);
+      })
+      .catch((error) => {
+        res.sendStatus(500);
+        console.log(error);
+      })
+  } else {
+    res.sendStatus(403);
+  }
+})
+
+router.delete('/locationStory/story/:id', (req, res) => {
+  console.log('DELETE /api/junction/locationStory/story/:id');
+  if (req.isAuthenticated) {
+    let query = `
+      DELETE FROM "locations_stories_junction"
+      WHERE "story_id" = $1
+      AND EXISTS (SELECT 1
+                  FROM "locations_stories_junction" as "ls"
+                  JOIN "stories" AS "s"
+                  ON "s"."id" = "ls"."story_id"
+                  JOIN "worlds" AS "w"
+                  ON "w"."id" = "s"."world_id"
+                  WHERE "ls"."story_id" = $1
+                  AND "w"."user_id" = $2)
+    `;
+    let params = [req.params.id, req.user.id];
+    pool.query(query, params)
+      .then(() => {
+        res.sendStatus(202);
+      })
+      .catch((error) => {
+        res.sendStatus(500);
+        console.log(error);
+      })
+  } else {
+    res.sendStatus(403);
+  }
+})
+
 module.exports = router;
--- src/redux/requests/junctionRequests.js
+++ src/redux/requests/junctionRequests.js
@@ -12,3 +12,30 @@ export function callLocationStoryJunction (payload) {
       throw error.response || error;
     })
 }
+
+export function callDeleteLSJunctionByLocation (id) {
+  const config = {
+    headers: { 'Content-Type': 'application/json' },
+    withCredentials: true,
+  };
+
+  return axios.delete(`/api/junction/locationStory/location/${id}`, config)
+    .then(response => response)
+    .catch((error) => {
+      throw error.response || error;
+    })
+}
+
+export function callDeleteLSJunctionByStory (id) {
+  const config = {
+    headers: { 'Content-Type': 'application/json' },
+    withCredentials: true,
+  };
+
+  return axios.delete(`/api/junction/locationStory/story/${id}`, config)
+    .then(response => response)
+    .catch((error) => {
+      throw error.response || error;
+    })
+}
+
