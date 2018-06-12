d8b9ba295ba22641fbf12558ecfd82146cbf3c1f
Kevin J Dexter <kevin.j.dexter@gmail.com>
2018-06-12 13:51:54 -0500

    event reducer created

---
+++ src/redux/reducers/eventReducer.js
@@ -0,0 +1,57 @@
+import { combineReducers } from 'redux';
+import { EVENT_ACTIONS } from '../actions/eventActions';
+
+const events = (state = [], action) => {
+  switch (action.type) {
+    case EVENT_ACTIONS.SET_EVENTS:
+      return action.payload;
+    default:
+      return state;
+  }
+}
+
+const isLoading = (state = false, action) => {
+  switch (action.type) {
+    case EVENT_ACTIONS.REQUEST_START:
+      return true;
+    case EVENT_ACTIONS.REQUEST_DONE:
+      return false
+    default: 
+      return state;
+  }
+}
+
+const eventsInWorld = (state = [], action) => {
+  switch (action.type) {
+    case EVENT_ACTIONS.SET_EVENTS_IN_WORLD:
+      return action.payload;
+    default:
+      return state;
+  }
+}
+
+const eventsInStory = (state = [], action) => {
+  switch (action.type) {
+    case EVENT_ACTIONS.SET_EVENTS_IN_STORY:
+      return action.payload;
+    default:
+      return state;
+  }
+}
+
+const eventDetails = (state = {stories: [], relationships: [], locations: []}, action) => {
+  switch (action.type) {
+    case EVENT_ACTIONS.SET_EVENT_DETAILS:
+      return action.payload;
+    default:
+      return state;
+  }
+}
+
+export default combineReducers ({
+  events,
+  isLoading,
+  eventsInWorld,
+  eventsInStory,
+  eventDetails,
+})
