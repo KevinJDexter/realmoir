929a02c09693944fa72217bdd5e2c1987a783f64
Kevin J Dexter <kevin.j.dexter@gmail.com>
2018-06-12 14:42:05 -0500

    remove locations from home page

--- src/redux/sagas/homeSaga.js
+++ src/redux/sagas/homeSaga.js
@@ -4,11 +4,11 @@ import { STORY_ACTIONS } from '../actions/storyActions';
 import { WORLD_ACTIONS } from '../actions/worldActions';
 import { LOCATION_ACTIONS } from '../actions/locationActions';
 import { RECENTLY_ADDED_ACTIONS } from '../actions/recentlyAddedActions';
+import { EVENT_ACTIONS } from '../actions/eventActions';
 
 function* startHome(action) {
   yield put ({ type: STORY_ACTIONS.GET_STORIES});
   yield put ({ type: WORLD_ACTIONS.GET_WORLDS});
-  yield put ({ type: LOCATION_ACTIONS.GET_LOCATIONS});
   yield put ({ type: RECENTLY_ADDED_ACTIONS.GET_RECENTLY_ADDED});
 }
 
