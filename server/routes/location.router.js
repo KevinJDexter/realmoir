f4a2f037662d7973b4f8361363ebcae2308663de
Kevin J Dexter <kevin.j.dexter@gmail.com>
2018-06-14 10:41:54 -0500

    envoke privacy in location router

--- server/routes/location.router.js
+++ server/routes/location.router.js
@@ -10,11 +10,19 @@ router.get('/', (req, res) => {
     FROM "locations" AS "l"
     JOIN "worlds" AS "w"
     ON "l"."world_id" = "w"."id"
+    JOIN "users" AS "u"
+    ON "u"."id" = "w"."user_id"
   `;
   let params = [];
   if (req.isAuthenticated()) {
-    query = query + ` WHERE "w"."user_id" = $1;`
+    query = query + ` WHERE "u"."id" = $1;`
     params.push(req.user.id);
+  } else {
+    query = query + ` 
+      WHERE "l"."is_private" = false
+      AND "w"."is_private" = false
+      AND "u"."content_private" = false
+    `;
   }
   pool.query(query, params)
     .then((results) => {
@@ -44,7 +52,55 @@ router.get('/search/general', (req, res) => {
   if (req.isAuthenticated()) {
     query = query + ` AND "u"."id" = $2`;
     params.push(req.user.id);
-  };
+  } else {
+    query = query + ` 
+      AND "l"."is_private" = false
+      AND "w"."is_private" = false
+      AND "u"."content_private" = false
+    `;
+  }
+  pool.query(query, params)
+    .then((results) => {
+      res.send(results.rows);
+    })
+    .catch((error) => {
+      res.sendStatus(500);
+      console.log(error);
+    })
+})
+
+// Gets detailed information about given location
+// Conceals private notes if not the logged in user
+router.get('/:id', (req, res) => {
+  console.log('GET /api/location/id');
+  let query = `
+    SELECT "l"."id", "l"."name", "l"."description", "l"."history", "l"."climate", "l"."img_url", "l"."world_id", "w"."name" as "world", "l"."date_created", "l"."is_private",
+    CASE WHEN "u"."id" = $1
+         THEN "l"."private_notes"
+         ELSE NULL
+         END AS "private_notes",
+    CASE WHEN "u"."id" = $1
+         THEN true
+         ELSE false
+         END AS "is_owner"
+    FROM "locations" AS "l"
+    JOIN "worlds" AS "w"
+    ON "l"."world_id" = "w"."id"
+    JOIN "users" AS "u"
+    ON "u"."id" = "w"."user_id"
+    WHERE "l"."id" = $2
+  `;
+  let userId = null;
+  if (req.isAuthenticated()) {
+    userId = req.user.id;
+  } else {
+    query = query + ` 
+        AND "l"."is_private" = false
+        AND "w"."is_private" = false
+        AND "u"."content_private" = false
+      `;
+  }
+  const params = [userId, req.params.id];
   pool.query(query, params)
     .then((results) => {
       res.send(results.rows);
@@ -59,11 +115,22 @@ router.get('/search/general', (req, res) => {
 router.get('/inWorld/:id', (req, res) => {
   console.log('GET /api/location/inWorld/id');
   let query = `
-    SELECT "id", "name", "description", "history", "climate"
-    FROM "locations"
-    WHERE "world_id" = $1;
+    SELECT "l"."id", "l"."name", "l"."description", "l"."history", "l"."climate", "l"."date_created"
+    FROM "locations" AS "l"
+    JOIN "worlds" AS "w"
+    ON "w"."id" = "l"."world_id"
+    JOIN "users" AS "u"
+    ON "u"."id" = "w"."user_id"
+    WHERE "w"."id" = $1
   `;
   const params = [req.params.id];
+  if (!req.isAuthenticated()) {
+    query = query + ` 
+      AND "l"."is_private" = false
+      AND "w"."is_private" = false
+      AND "u"."content_private" = false
+    `;
+  }
   pool.query(query, params)
     .then((results) => {
       res.send(results.rows);
@@ -82,9 +149,20 @@ router.get('/inStory/:id', (req, res) => {
     FROM "locations" AS "l"
     JOIN "locations_stories_junction" AS "ls"
     ON "ls"."location_id" = "l"."id"
+    JOIN "worlds" AS "w"
+    ON "w"."id" = "l"."world_id"
+    JOIN "users" AS "u"
+    ON "u"."id" = "w"."user_id"
     WHERE "ls"."story_id" = $1
   `;
   const params = [req.params.id];
+  if (!req.isAuthenticated()) {
+    query = query + ` 
+      AND "l"."is_private" = false
+      AND "w"."is_private" = false
+      AND "u"."content_private" = false
+    `;
+  }
   pool.query(query, params)
     .then((results) => {
       res.send(results.rows);
@@ -112,11 +190,22 @@ router.get('/neighbors/:id', (req, res) => {
     JOIN "neighboring_locations" AS "nl"
     ON "nl"."first_location" = "l"."id"
     OR "nl"."second_location" = "l"."id"
+    JOIN "worlds" AS "w"
+    ON "w"."id" = "l"."world_id"
+    JOIN "users" AS "u"
+    ON "u"."id" = "w"."user_id"
     WHERE ("nl"."first_location" = $1
     OR "nl"."second_location" = $1)
-    AND "l"."id" != $1;
+    AND "l"."id" != $1
   `;
   let params = [req.params.id];
+  if (!req.isAuthenticated()) {
+    query = query + ` 
+      AND "l"."is_private" = false
+      AND "w"."is_private" = false
+      AND "u"."content_private" = false
+    `;
+  }
   pool.query(query, params)
     .then((results) => {
       res.send(results.rows);
@@ -135,9 +224,20 @@ router.get('/character/:id', (req, res) => {
     FROM "locations" AS "l"
     JOIN "characters_locations_junction" AS "cl"
     ON "cl"."location_id" = "l"."id"
-    WHERE "cl"."character_id" = $1;
+    JOIN "worlds" AS "w"
+    ON "w"."id" = "l"."world_id"
+    JOIN "users" AS "u"
+    ON "u"."id" = "w"."user_id"
+    WHERE "cl"."character_id" = $1
   `;
   let params = [req.params.id];
+  if (!req.isAuthenticated()) {
+    query = query + ` 
+      AND "l"."is_private" = false
+      AND "w"."is_private" = false
+      AND "u"."content_private" = false
+    `;
+  }
   pool.query(query, params)
     .then((results) => {
       res.send(results.rows);
@@ -154,11 +254,28 @@ router.post('/', (req, res) => {
   if (req.isAuthenticated()) {
     const location = req.body;
     let query = `
-      INSERT INTO "locations" ("name", "description", "history", "climate", "img_url", "private_notes", "world_id")
-      VALUES ($1, $2, $3, $4, $5, $6, $7)
+      INSERT INTO "locations" (
+        "name", 
+        "description", 
+        "history", 
+        "climate", 
+        "img_url", 
+        "private_notes", 
+        "world_id", 
+        "is_private")
+      VALUES ($1, $2, $3, $4, $5, $6, $7, $8)
       RETURNING "id";
     `;
-    let params = [location.name, location.description, location.history, location.climate, location.img_url, location.private_notes, location.world_id];
+    let params = [
+      location.name, 
+      location.description, 
+      location.history, 
+      location.climate, 
+      location.img_url, 
+      location.private_notes, 
+      location.world_id,
+      location.is_private
+    ];
     pool.query(query, params)
       .then((results) => {
         console.log(results);
@@ -171,42 +288,6 @@ router.post('/', (req, res) => {
   } else {
     res.sendStatus(403);
   }
-})
-
-// Gets detailed information about given location
-// Conceals private notes if not the logged in user
-router.get('/:id', (req, res) => {
-  console.log('GET /api/location/id');
-  let query = `
-    SELECT "l"."id", "l"."name", "l"."description", "l"."history", "l"."climate", "l"."img_url", "l"."world_id", "w"."name" as "world", "l"."date_created",
-    CASE WHEN "u"."id" = $1
-         THEN "l"."private_notes"
-         ELSE NULL
-         END AS "private_notes",
-    CASE WHEN "u"."id" = $1
-         THEN true
-         ELSE false
-         END AS "is_owner"
-    FROM "locations" AS "l"
-    JOIN "worlds" AS "w"
-    ON "l"."world_id" = "w"."id"
-    JOIN "users" AS "u"
-    ON "u"."id" = "w"."user_id"
-    WHERE "l"."id" = $2
-  `;
-  let userId = null;
-  if (req.isAuthenticated()) {
-    userId = req.user.id;
-  }
-  const params = [userId, req.params.id];
-  pool.query(query, params)
-    .then((results) => {
-      res.send(results.rows);
-    })
-    .catch((error) => {
-      res.sendStatus(500);
-      console.log(error);
-    })
 })
 
 // Deletes the given location from the database
@@ -250,8 +331,8 @@ router.put('/:id', (req, res) => {
           "history" = $3, 
           "climate" = $4, 
           "img_url" = $5, 
-          "private_notes" = $6, 
-          "world_id" = $7
+          "private_notes" = $6,
+          "is_private" = $7
       WHERE "id" = $8
       AND EXISTS (SELECT 1
                   FROM "locations" AS "l"
@@ -259,7 +340,16 @@ router.put('/:id', (req, res) => {
                   ON "w"."id" = "l"."world_id"
                   WHERE "l"."id" = $8 AND "w"."user_id" = $9);
     `;
-    const params = [update.name, update.description, update.history, update.climate, update.img_url, update.private_notes, update.world_id, req.params.id, req.user.id];
+    const params = [
+      update.name, 
+      update.description, 
+      update.history, 
+      update.climate, 
+      update.img_url, 
+      update.private_notes, 
+      update.is_private, 
+      req.params.id, 
+      req.user.id];
     pool.query(query, params)
       .then(() => {
         res.sendStatus(202);
